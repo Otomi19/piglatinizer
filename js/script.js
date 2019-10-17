3ec9d56abe049fcba9f015d393485d510cc5832d
Otomi19 <oo6661@thewcs.org>
2019-10-17 21:56:15 +0000

    changed jv and css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+    $("#button").click(function(){
+        console.log("hello")
+    });
 
 
-  
-  
-  
-  
+
+
 });
