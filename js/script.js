9b26f35e2bcb54b4342995e9755d6ff1cef5351b
Otomi19 <oo6661@thewcs.org>
2019-10-24 21:58:55 +0000

    you worked on the vowels

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
     $("#button").click(function(){
-        console.log("hello")
-    });
-
-
-
-
+    var inputText = $(".input").val();
+        // console.log('test')
+       // alert("hello")
+      console.log(inputText.charAt(0));
+      var vowels=["a","e","i","o","u"];
+    var firstCharIndex= vowels.indexOf(inputText.charAt(0));
+    //first character is a consonat
+    if(firstCharIndex==-1){
+    $(".output").text()
+    }
+    // first character is a vowel
+    else{
+     $(".output").text(inputText+"ay");
+    }
+  });
 });
